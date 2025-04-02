76f084bc6b1f0c1f0ea09e2c0884ed282d6ec962
Hyub Kim <98584348+Hyubbbb@users.noreply.github.com>
2025-04-02 16:56:58 +0900

    [Bronze I] Title: 소인수분해, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,6 +1,12 @@
 n = int(input())
 
-for i in range(2, n+1):
+i = 2
+while i**2 <= n:
+# for i in range(2, n+1):
     while n%i == 0:
-        n = n//i
         print(i)
+        n = n//i
+    i += 1
+
+if n > 1:
+    print(n)
